96ee42f2466f88273c91e4b84d30473239b4f626
Anton Romankov <ar@askque.ru>
2020-05-14 23:53:23 +0300

    Optimization: html and css; cross-env NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,30 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+console.log(isDev)
+console.log(isProd)
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
+
+    if (isProd){
+        config.minimizer=[
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -17,13 +41,10 @@ module.exports = {
             '@': path.resolve(__dirname, './src')
         }
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: isDev
     },
     output: {
         filename: '[name].[contenthash].js',
@@ -31,7 +52,10 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
@@ -39,13 +63,22 @@ module.exports = {
                 from: path.resolve(__dirname, './src/favicon.ico'),
                 to: path.resolve(__dirname, 'dist')
             }
-        ])
+        ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: isDev,
+                        reloadAll: true
+                    },
+                }, 'css-loader']
             },
             {
                 test: /\.(png|jpeg|svg|gif)$/,
